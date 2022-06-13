1c4c4759a77d0547a329f9526b5a826907566642
Gokulcasper <ronitcasper215@gmail.com>
2022-06-13 10:05:35 +0530

    Feat : Scope

--- script.js
+++ script.js
@@ -8,10 +8,9 @@
 // {
 //   let val = 2;
 //   console.log(val);
-// }
+// } // let is a Block Scope
 
 let user = "Raj";
-
 (function greetuser() {
   let user = "Gokul";
   let msg = "Hello " + user;
@@ -21,3 +20,13 @@ function log(msg) {
   console.log(msg);
 } // Nested Function is possible
 console.log(user);
+
+//Function Scope
+
+// (function displayNumber() {
+//   for (i = 1; i <= 10; ++i) {
+//     // In Function var = i (Default) if suppose, let=i means its change to block scope
+//     console.log(`Value of Number is : ${i}`);
+//   }
+// })();
+// console.log(i);
